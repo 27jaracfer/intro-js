41800c851e4def4e4634bc2187f28b2d959e804e
27jaracfer <27jaracfer@washk12.org>
2025-02-19 11:40:15 -0700

    Gaming dev 2/19

---
+++ input.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+// Get user input for character name and game
+//let characterName = prompt("What is your name? ");
+//let gameName = prompt("What's your favorite game? ");
+
+// Combine the inputs into a greeting
+//let greeting = "Welcome to " + gameName + ", " + characterName + "! Prepare for your adventure!";
+
+//console.log(greeting);
+
+
+let cardsuit = prompt(" What card suit do you pick? ");
+let cardrank = prompt("What card rank do you pick? ");
+
+console.log("Your favorit card is 3H"+ cardsuit+cardrank)
