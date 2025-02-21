9736b8abc67a62d579d5e7ad87aa926f533b000e
27jaracfer <27jaracfer@washk12.org>
2025-02-21 10:38:20 -0700

    Mario, and Luigi Story gam dev

---
+++ madlibs.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")(); // Ask for user inputs
+let gameCharacter = prompt(" pick a character nane: ");
+let gameCharacter2 = prompt("pick a character nane: ");
+let gameCharacter3 = prompt("pick a character nane: ");
+let gameCharacter4 = prompt("pick a character nane: ");
+let weapon = prompt("pick a weapon nane: ");
+let story2 = " Once upon a time Princess Peach was resting in her room waiting for Mario to return " + gameCharacter + " but then a fire ball blasts threw Princess Peach window she look over it's Bowser, he quickly grabs and takes her away. ..."
+let story = "Once upon a time Mario, and Luigi were taking there daily walk around the castle but then they heard a loud blast coming from the direction of the castle, Mario and Luigi ran into the castle to Princess Peach is room. " + gameCharacter + " Mario looks around and finds a note form Bowser reading come and find me. So mario, and Luigi set off on another adventure to save princess Peach ..."
+console.log(story);
