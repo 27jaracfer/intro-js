98c50cfd4337c61839c201baf8e968acf7d2aa09
27jaracfer <27jaracfer@washk12.org>
2025-02-25 11:31:11 -0700

    Gaming Dev story

--- madlibs.js
+++ madlibs.js
@@ -1,9 +1,12 @@
 const prompt = require("prompt-sync")(); // Ask for user inputs
-let gameCharacter = prompt(" pick a character nane: ");
-let gameCharacter2 = prompt("pick a character nane: ");
-let gameCharacter3 = prompt("pick a character nane: ");
-let gameCharacter4 = prompt("pick a character nane: ");
-let weapon = prompt("pick a weapon nane: ");
-let story2 = " Once upon a time Princess Peach was resting in her room waiting for Mario to return " + gameCharacter + " but then a fire ball blasts threw Princess Peach window she look over it's Bowser, he quickly grabs and takes her away. ..."
-let story = "Once upon a time Mario, and Luigi were taking there daily walk around the castle but then they heard a loud blast coming from the direction of the castle, Mario and Luigi ran into the castle to Princess Peach is room. " + gameCharacter + " Mario looks around and finds a note form Bowser reading come and find me. So mario, and Luigi set off on another adventure to save princess Peach ..."
+let gameCharacter = prompt(" pick a charcter : ");
+let gameCharacter2 = prompt("pick a charcter : ");
+let gameCharacter3 = prompt("pick a charcter : ");
+let gameCharacter4 = prompt("pick a charcter : ");
+let gameCharacter5 = prompt("pick a charcter : ");
+let gameCharacter6 = prompt("pick a charcter: ");
+let weapon = prompt("pick a weapon: ");
+let weapon2 = prompt("pick a weapon: ");
+let story2 = " Once upon a time "+gameCharacter4+" was resting in her room waiting for "+gameCharacter+" to return  but then a "+weapon+" blasts threw "+gameCharacter4+" window she look over it's "+gameCharacter5+", and his evil son "+gameCharacter6+", they quickly grab her. "+gameCharacter4+" uses her "+weapon2+" to try to fight back but "+gameCharacter5+", and "+gameCharacter6+" where to strong so then they quicky grab "+gameCharacter4+"  and toke her away. ..."
+let story = "Once upon a time "+gameCharacter+", "+gameCharacter2+", and "+gameCharacter3+" were taking there daily walk around the castle but then they heard a loud blast coming from the direction of the castle, "+gameCharacter+", "+gameCharacter2+", and "+gameCharacter3+" ran into the castle to "+gameCharacter4+" is room. They look around and find her missing,  then finds a note form "+gameCharacter5+", and "+gameCharacter6+" reading come and find us. So "+gameCharacter+", "+gameCharacter2+", and "+gameCharacter3+" set off on another adventure to save "+gameCharacter4+" ..."
 console.log(story);
